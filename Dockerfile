7a91db22126a36bdf6363848f5ee6d98fd56aaff
Esteban Borai <estebanborai@outlook.com>
2019-07-06 00:25:59 -0300

    Add CSV seed

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ENV MYSQL_USER admin
 ENV MYSQL_PASSWORD root
 ENV MYSQL_PORT 3306
 
+COPY data/ /docker-entrypoint-initdb.d/raw-data/
 COPY init.sql /docker-entrypoint-initdb.d/init.sql
 
 EXPOSE 3306:3306
--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 USE pancakes;
 
--- seeds tables
+-- seeds tables `customers_northwind`
 CREATE TABLE IF NOT EXISTS `customers_northwind` (
 	`customer_id` int(11) NOT NULL AUTO_INCREMENT,
 	`customer_name` varchar(255) DEFAULT NULL,
@@ -11,3 +11,18 @@ CREATE TABLE IF NOT EXISTS `customers_northwind` (
 	`country` varchar(255) DEFAULT NULL,
 	PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/raw-data/customers_northwind.csv'
+INTO TABLE customers_northwind
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS (
+	customer_id,
+	customer_name,
+	contact_name,
+	address,
+	city,
+	postal_code,
+	country
+);"
